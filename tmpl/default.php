a8f8e90613ac80f798b212d71a0e3e81ce6a204f
progreccor <boss@delo-design.ru>
2026-04-23 15:12:11 +0300

    RadicalForm 4.0.0 prepare RadicalForm 4.0.0 for Joomla 6

--- tmpl/default.php
+++ tmpl/default.php
@@ -9,6 +9,11 @@
  */
 
 defined('_JEXEC') or die;
+
+/** @var string $subject */
+/** @var string $header */
+/** @var string $mainbody */
+/** @var string $footer */
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><!--[if gte mso 15]>
     <xml>
